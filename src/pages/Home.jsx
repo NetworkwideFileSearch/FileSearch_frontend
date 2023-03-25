2f093ec4f4295ae3fca5a9d689361a677810fa4b
Pranav Mohril <pranavmohril@gmail.com>
2023-03-25 14:13:10 +0530

    react-icons library added, search bar added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,46 @@
 import React from "react";
-
+import { Input, useTheme } from "@nextui-org/react";
+import { FaSearch } from "react-icons/fa";
 const Home = () => {
-  return <div>Home</div>;
+  const { theme } = useTheme();
+  const handleSearch = () => {
+    console.log("Search Button clicked");
+    alert("To be implemented!");
+  };
+  return (
+    <div
+      className="container"
+      style={{ position: "relative", height: "100vh", width: "100%" }}
+    >
+      <div
+        style={{
+          position: "absolute",
+          top: "25%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          width: "50%",
+        }}
+      >
+        <Input
+          placeholder="Search for a file"
+          width="100%"
+          aria-label="Search for a file"
+          style={{ paddingLeft: "2rem" }}
+          contentRightStyling={false}
+          contentRight={
+            <FaSearch
+              style={{ cursor: "pointer", marginRight: "1rem" }}
+              onClick={() => {
+                console.log("first");
+                handleSearch();
+              }}
+              color={theme.colors.accents5}
+            />
+          }
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
