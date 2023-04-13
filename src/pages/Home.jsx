1af4a573e0cf58dd08f6cb317e9995ebdd6d5c48
Sanket <sakulkarni0801@gmail.com>
2023-04-14 00:00:37 +0530

    Added table & history

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,172 @@
 import React from "react";
-import { Input, useTheme } from "@nextui-org/react";
+import {
+  Card,
+  Container,
+  Input,
+  Table,
+  Text,
+  useTheme,
+} from "@nextui-org/react";
 import { FaSearch } from "react-icons/fa";
+
+const columns = [
+  {
+    key: "name",
+    label: "File Name",
+  },
+  {
+    key: "host",
+    label: "Host",
+  },
+  {
+    key: "location",
+    label: "File Location",
+  },
+  {
+    key: "size",
+    label: "Size",
+  },
+  {
+    key: "action",
+    label: "Action",
+  },
+];
+
+const rows = [
+  {
+    key: "1",
+    name: "Resume.pdf",
+    host: "192.168.143.27",
+    location: "D:/Me/Placement/Resume/Resume.pdf",
+    size: "9MB",
+  },
+  {
+    key: "2",
+    name: "Me.pdf",
+    host: "192.168.45.27",
+    location: "D:/Me/Placement/Resume/Resume.pdf",
+    size: "12MB",
+  },
+  {
+    key: "3",
+    name: "Resume.pdf",
+    host: "100.168.143.27",
+    location: "D:/Me/Placement/Resume/Resume.pdf",
+    size: "9MB",
+  },
+  {
+    key: "4",
+    name: "Me.pdf",
+    host: "192.168.143.27",
+    location: "D:/Me/Placement/Resume/Resume.pdf",
+    size: "12MB",
+  },
+  {
+    key: "5",
+    name: "Resume.pdf",
+    host: "192.78.143.27",
+    location: "D:/Me/Placement/Resume/Resume.pdf",
+    size: "12MB",
+  },
+  {
+    key: "6",
+    name: "Hello.pdf",
+    host: "192.168.143.27",
+    location: "D:/Me/Placement/Resume/Resume.pdf",
+    size: "8MB",
+  },
+  {
+    key: "7",
+    name: "Resume.pdf",
+    host: "192.168.143.27",
+    location: "D:/Me/Placement/Resume/Resume.pdf",
+    size: "12MB",
+  },
+  {
+    key: "8",
+    name: "No.pdf",
+    host: "192.168.143.27",
+    location: "D:/Me/Placement/Resume/Resume.pdf",
+    size: "8MB",
+  },
+  {
+    key: "9",
+    name: "Resume.pdf",
+    host: "192.168.143.27",
+    location: "D:/Me/Placement/Resume/Resume.pdf",
+    size: "12MB",
+  },
+  {
+    key: "10",
+    name: "Resume.pdf",
+    host: "192.168.143.27",
+    location: "D:/Me/Placement/Resume/Resume.pdf",
+    size: "8MB",
+  },
+  {
+    key: "11",
+    name: "Resume.pdf",
+    host: "192.168.143.27",
+    location: "D:/Me/Placement/Resume/Resume.pdf",
+    size: "12MB",
+  },
+  {
+    key: "12",
+    name: "Resume.pdf",
+    host: "192.168.143.27",
+    location: "D:/Me/Placement/Resume/Resume.pdf",
+    size: "8MB",
+  },
+  {
+    key: "13",
+    name: "Resume.pdf",
+    host: "192.168.143.27",
+    location: "D:/Me/Placement/Resume/Resume.pdf",
+    size: "12MB",
+  },
+];
+
+const history = [
+  {
+    key: "1",
+    name: "Resume 1.pdf",
+  },
+  {
+    key: "2",
+    name: "Resume 6.pdf",
+  },
+  {
+    key: "3",
+    name: "Resume 5.pdf",
+  },
+  {
+    key: "4",
+    name: "Resume 4.pdf",
+  },
+  {
+    key: "5",
+    name: "Resume 2.pdf",
+  },
+];
+
 const Home = () => {
   const { theme } = useTheme();
+
   const handleSearch = () => {
     console.log("Search Button clicked");
     alert("To be implemented!");
   };
+
   return (
-    <div
-      className="container"
-      style={{ position: "relative", height: "100vh", width: "100%" }}
-    >
+    <Container style={{ position: "relative", width: "100%" }}>
+      {/* Search bar */}
       <div
         style={{
-          position: "absolute",
-          top: "25%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
+          marginTop: "5%",
+          marginLeft: "25%",
+          marginBottom: "3%",
           width: "50%",
-        }}
-      >
+        }}>
         <Input
           placeholder="Search for a file"
           width="100%"
@@ -39,7 +185,67 @@ const Home = () => {
           }
         />
       </div>
-    </div>
+      {/* Table */}
+      {/* <div>
+        <Table
+          bordered
+          shadow={false}
+          color="secondary"
+          aria-label="Example pagination  table"
+          css={{
+            height: "auto",
+            minWidth: "100%",
+          }}
+          selectionMode="multiple">
+          <Table.Header columns={columns}>
+            {(column) => (
+              <Table.Column key={column?.key}>{column?.label}</Table.Column>
+            )}
+          </Table.Header>
+          <Table.Body items={rows}>
+            {(item) => (
+              <Table.Row key={item?.key}>
+                {(columnKey) => <Table.Cell>{item[columnKey]}</Table.Cell>}
+              </Table.Row>
+            )}
+          </Table.Body>
+          <Table.Pagination
+            shadow
+            noMargin
+            align="center"
+            rowsPerPage={8}
+            onPageChange={(page) => console.log({ page })}
+          />
+        </Table>
+      </div> */}
+      {/* History */}
+      <div>
+        <Text h3>History</Text>
+        <div
+          style={{
+            height: "0.2vh",
+            width: "100%",
+            backgroundColor: theme.colors.white,
+          }}
+        />
+        <div
+          style={{
+            marginTop: "2%",
+          }}>
+          {history.map((histElement) => (
+            <Card
+              key={histElement?.key}
+              isPressable
+              variant="bordered"
+              css={{ mw: "100%", mb: "$10" }}>
+              <Card.Body>
+                <Text>{histElement?.name}</Text>
+              </Card.Body>
+            </Card>
+          ))}
+        </div>
+      </div>
+    </Container>
   );
 };
 
--- src/shared/theme.js
+++ src/shared/theme.js
@@ -1,7 +1,7 @@
 import { createTheme } from "@nextui-org/react";
 
 const theme = createTheme({
-  type: "light", // light / dark
+  type: "dark", // light / dark
   className: "",
   theme: {
     colors: {},
